bc609f423700e6bbf104a5fae1b74414b00a6762
professionalbot <135655483+nouman531@users.noreply.github.com>
2023-06-19 23:44:55 +0500

    Adding_Student Class

---
+++ app/src/main/java/com/example/assignment02/DBHandler.java
@@ -0,0 +1,73 @@
+package com.example.assignment02;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    private final Context context;
+    private static final String DBName="Mydb";
+    private static final int DBVersion=1;
+    private static final String Table_Name="My_Table";
+
+    private static final String ID="id";
+    private static final String Name="std_name";
+    private static final String Age="std_age";
+    private static final String Class="std_class";
+
+
+    public DBHandler(@Nullable Context context, Context context1) {
+        super(context, DBName, null, DBVersion);
+        this.context = context1;
+    }
+
+
+
+
+    @Override
+    public void onCreate(@NonNull SQLiteDatabase db) {
+        String query="CREATE TABLE "+ Table_Name +
+                "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                Name + " TEXT, " +
+                Age + " TEXT, " +
+                Class + " TEXT);";
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(@NonNull SQLiteDatabase db, int i, int i1) {
+        String query="DROP TABLE IF EXISTS "+ Table_Name;
+        db.execSQL(query);
+        onCreate(db);
+
+    }
+
+    public void AddStudent(String name,String age,String cls){
+        SQLiteDatabase db= this.getWritableDatabase();//so we can add the values in the data
+        ContentValues cv=new ContentValues();
+        cv.put(Name,name);
+        cv.put(Age,age);
+        cv.put(Class,cls);
+
+        long result=db.insert(Table_Name,null,cv);
+
+        if(result==-1){
+            Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context,"Success",Toast.LENGTH_SHORT).show();
+        }
+        db.close();
+
+    }
+}
--- app/src/main/java/com/example/assignment02/MainActivity.java
+++ app/src/main/java/com/example/assignment02/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.assignment02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    EditText editText;
+    Button searchbtn,addbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recycleview);
+        editText=findViewById(R.id.search);
+        searchbtn=findViewById(R.id.button);
+        addbtn=findViewById(R.id.addstd);
+
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,AddStudent.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
